efed45c664a1ae8aaed1acddbfbbe0c2265a1d5f
Ricardo Branco <101410949+Rickbranco@users.noreply.github.com>
2022-03-28 13:03:31 -0300

    Adicionado Arquivos

---
+++ script.js
@@ -0,0 +1,582 @@
+let carta1 = {
+  nome: "Spawn",
+  foto:
+    "https://cdn.ome.lt/1_8a3vI-EQPXMrVkb__6Iywt1xM=/1200x630/smart/extras/conteudos/spawn-capa.png",
+  atributos: {
+    Força: 9,
+    Inteligência: 6,
+    Tecnologia: 6,
+    Velocidade: 6,
+    Liderança: 5,
+    Agilidade: 9
+  }
+};
+
+let carta2 = {
+  nome: "Anti-Spawn",
+  foto:
+    "https://www.superherodb.com/pictures2/portraits/10/050/1071.jpg?v=1554538681",
+  atributos: {
+    Força: 9,
+    Inteligência: 8,
+    Tecnologia: 1,
+    Velocidade: 6,
+    Liderança: 6,
+    Agilidade: 8
+  }
+};
+let carta3 = {
+  nome: "Ângela",
+  foto:
+    "https://s3.amazonaws.com/comicgeeks/characters/avatars/16211.jpg?t=1644379074",
+  atributos: {
+    Força: 8,
+    Inteligência: 7,
+    Tecnologia: 2,
+    Velocidade: 5,
+    Liderança: 6,
+    Agilidade: 8
+  }
+};
+let carta4 = {
+  nome: "Chacina",
+  foto:
+    "https://pm1.narvii.com/6447/36c812cc2f14f5fb0ebcc5e469ce081bbe454385_hq.jpg",
+  atributos: {
+    Força: 8,
+    Inteligência: 2,
+    Tecnologia: 9,
+    Velocidade: 3,
+    Liderança: 1,
+    Agilidade: 2
+  }
+};
+let carta5 = {
+  nome: "Violador",
+  foto:
+    "https://pm1.narvii.com/6546/37ddebeb6bb314ecffd730091676a87b9d0f0b1a_hq.jpg",
+  atributos: {
+    Força: 9,
+    Inteligência: 5,
+    Tecnologia: 1,
+    Velocidade: 4,
+    Liderança: 2,
+    Agilidade: 5
+  }
+};
+let carta6 = {
+  nome: "Superman",
+  foto:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA2gYVf35N-h-XFZEDCtqdtUkbXzEXloMCUuMLkVh8PRSrx9ZeVK5ow9YUhRqFaCmKtsM&usqp=CAU",
+  atributos: {
+    Força: 10,
+    Inteligência: 8,
+    Tecnologia: 1,
+    Velocidade: 10,
+    Liderança: 9,
+    Agilidade: 9
+  }
+};
+
+let carta7 = {
+  nome: "Ajax",
+  foto:
+    "https://i0.wp.com/supernovo.net/wp-content/uploads/2014/03/cacador-de-marte-700x400.png",
+  atributos: {
+    Força: 8,
+    Inteligência: 7,
+    Tecnologia: 5,
+    Velocidade: 8,
+    Liderança: 9,
+    Agilidade: 6
+  }
+};
+
+let carta8 = {
+  nome: "Flash",
+  foto:
+    "https://t.ctcdn.com.br/WTyCxjbstCNMh0duWI4VELHkH7Q=/1280x720/smart/filters:format(webp)/i537145.jpeg",
+  atributos: {
+    Força: 6,
+    Inteligência: 5,
+    Tecnologia: 4,
+    Velocidade: 10,
+    Liderança: 3,
+    Agilidade: 10
+  }
+};
+
+let carta9 = {
+  nome: "Lanterna Verde",
+  foto:
+    "https://conteudo.imguol.com.br/c/entretenimento/e4/2017/09/05/lanterna-verde-1504622200577_v2_450x337.jpg.webp",
+  atributos: {
+    Força: 9,
+    Inteligência: 5,
+    Tecnologia: 10,
+    Velocidade: 8,
+    Liderança: 2,
+    Agilidade: 6
+  }
+};
+
+let carta10 = {
+  nome: "Aquaman",
+  foto:
+    "https://observatoriodocinema.uol.com.br/wp-content/uploads/2018/09/lista-aquaman-4-bastidores.jpg",
+  atributos: {
+    Força: 8,
+    Inteligência: 7,
+    Tecnologia: 5,
+    Velocidade: 7,
+    Liderança: 8,
+    Agilidade: 8
+  }
+};
+
+let carta11 = {
+  nome: "Homem-Borracha",
+  foto:
+    "https://cinemacomrapadura.com.br/imagens/2020/12/20201207-homem-borracha-_filme-em-desenvolvimento.jpg",
+  atributos: {
+    Força: 4,
+    Inteligência: 4,
+    Tecnologia: 3,
+    Velocidade: 3,
+    Liderança: 2,
+    Agilidade: 7
+  }
+};
+
+let carta12 = {
+  nome: "Mulher-Gato",
+  foto:
+    "http://pm1.narvii.com/6497/c6d32e7648c9fbc053a825a15bf9b7a578627735_00.jpg",
+  atributos: {
+    Força: 4,
+    Inteligência: 7,
+    Tecnologia: 4,
+    Velocidade: 5,
+    Liderança: 3,
+    Agilidade: 8
+  }
+};
+
+let carta13 = {
+  nome: "Mulher-Maravilha",
+  foto:
+    "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2020/03/legiao_RJwQn4UT5IuA.jpg.jpeg",
+  atributos: {
+    Força: 9,
+    Inteligência: 6,
+    Tecnologia: 2,
+    Velocidade: 9,
+    Liderança: 6,
+    Agilidade: 8
+  }
+};
+
+let carta14 = {
+  nome: "Supergirl",
+  foto:
+    "https://cdn.ome.lt/C4oiTOu_sKd_K8nIl-NceTNNls4=/1200x630/smart/extras/conteudos/supergirl_OM5wpbM.jpg",
+  atributos: {
+    Força: 8,
+    Inteligência: 6,
+    Tecnologia: 1,
+    Velocidade: 8,
+    Liderança: 3,
+    Agilidade: 6
+  }
+};
+
+let carta15 = {
+  nome: "Superboy",
+  foto:
+    "https://tm.ibxk.com.br/ms/images/highlights/000/048/877/46118.jpg?ims=1200x675",
+  atributos: {
+    Força: 8,
+    Inteligência: 5,
+    Tecnologia: 2,
+    Velocidade: 8,
+    Liderança: 2,
+    Agilidade: 6
+  }
+};
+
+let carta16 = {
+  nome: "Superciborgue",
+  foto:
+    "https://pm1.narvii.com/6596/0fb4c56fc0a637a42530fd526f99f3e7aa9257ce_hq.jpg",
+  atributos: {
+    Força: 7,
+    Inteligência: 7,
+    Tecnologia: 9,
+    Velocidade: 7,
+    Liderança: 4,
+    Agilidade: 5
+  }
+};
+
+let carta17 = {
+  nome: "Batman",
+  foto:
+    "https://pop.proddigital.com.br/wp-content/uploads/sites/8/2021/11/batman.jpg",
+  atributos: {
+    Força: 7,
+    Inteligência: 10,
+    Tecnologia: 9,
+    Velocidade: 7,
+    Liderança: 10,
+    Agilidade: 8
+  }
+};
+
+let carta18 = {
+  nome: "Asa Noturna",
+  foto:
+    "https://www.multiversos.com.br/wp-content/uploads/2019/11/CapaPost-scaled.jpg",
+  atributos: {
+    Força: 5,
+    Inteligência: 8,
+    Tecnologia: 7,
+    Velocidade: 5,
+    Liderança: 8,
+    Agilidade: 7
+  }
+};
+
+let carta19 = {
+  nome: "Robin",
+  foto:
+    "https://conteudo.imguol.com.br/c/entretenimento/0f/2021/08/10/robin-de-tim-drake-se-assumiu-bissexual-em-nova-hq-do-batman-1628608960068_v2_1x1.jpg",
+  atributos: {
+    Força: 4,
+    Inteligência: 8,
+    Tecnologia: 8,
+    Velocidade: 4,
+    Liderança: 4,
+    Agilidade: 8
+  }
+};
+
+let carta20 = {
+  nome: "Azrael",
+  foto: "https://s.aficionados.com.br/imagens/azrael-jean-paul-valley.jpg",
+  atributos: {
+    Força: 7,
+    Inteligência: 5,
+    Tecnologia: 7,
+    Velocidade: 5,
+    Liderança: 2,
+    Agilidade: 7
+  }
+};
+
+let carta21 = {
+  nome: "Coringa",
+  foto:
+    "https://rollingstone.uol.com.br/media/_versions/corinha-hq-quadrinho-origem-batman-90-reproducao_widelg.jpg",
+  atributos: {
+    Força: 3,
+    Inteligência: 8,
+    Tecnologia: 6,
+    Velocidade: 3,
+    Liderança: 7,
+    Agilidade: 5
+  }
+};
+
+let carta22 = {
+  nome: "Bane",
+  foto:
+    "https://www.einerd.com.br/wp-content/uploads/2020/06/Bane-poderes-capa.jpg",
+  atributos: {
+    Força: 8,
+    Inteligência: 7,
+    Tecnologia: 2,
+    Velocidade: 3,
+    Liderança: 5,
+    Agilidade: 4
+  }
+};
+
+let carta25 = {
+  nome: "Apocalypse",
+  foto:
+    "http://pm1.narvii.com/6880/a607e72218e79c110b974657cceb4a35dc51a78cr1-1987-1650v2_00.jpg",
+  atributos: {
+    Força: 10,
+    Inteligência: 4,
+    Tecnologia: 1,
+    Velocidade: 4,
+    Liderança: 1,
+    Agilidade: 6
+  }
+};
+
+let carta23 = {
+  nome: "Lex Luthor",
+  foto:
+    "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2020/11/legiao_X2j6F5oSC43q.png.jpeg",
+  atributos: {
+    Força: 4,
+    Inteligência: 10,
+    Tecnologia: 8,
+    Velocidade: 3,
+    Liderança: 9,
+    Agilidade: 3
+  }
+};
+
+let carta24 = {
+  nome: "Aço",
+  foto:
+    "https://pm1.narvii.com/6787/58ec965580bfa283bf019230bb3c8a7c56635b12v2_hq.jpg",
+  atributos: {
+    Força: 7,
+    Inteligência: 9,
+    Tecnologia: 8,
+    Velocidade: 8,
+    Liderança: 4,
+    Agilidade: 5
+  }
+};
+
+let baralho = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta25,
+  carta23,
+  carta24
+];
+
+document.getElementById("btnZerar").disabled = true;
+
+let meuBaralho = [];
+
+function distribuiCartas() {
+  for (let minhasCartas = 0; minhasCartas < 12; minhasCartas++) {
+    let indiceAleatorio = Math.floor(Math.random() * baralho.length);
+
+    if (
+      indiceAleatorio >= baralho.length &&
+      indiceAleatorio >= baralho.length
+    ) {
+      indiceAleatorio = baralho.length - 1;
+    }
+
+    meuBaralho.push(baralho.splice(indiceAleatorio, 1)[0]);
+  }
+}
+
+distribuiCartas();
+
+let cartaMaquina;
+let cartaJogador;
+let minhaCarta = document.querySelector("#cartaEscolhida");
+let campoJogador = document.querySelector(".cartaJogador");
+let campoMaquina = document.querySelector(".cartaMaquina");
+let pontos = document.querySelector(".pontuacao");
+let versus = document.querySelector(".versus");
+
+pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+
+function sortearCarta() {
+  numeroCartaMaquina = Math.floor(Math.random() * baralho.length);
+  numeroCartaJogador = Math.floor(Math.random() * meuBaralho.length);
+
+  if (
+    numeroCartaMaquina >= baralho.length &&
+    numeroCartaJogador >= meuBaralho.length
+  ) {
+    numeroCartaMaquina = baralho.length - 1;
+  }
+  cartaMaquina = baralho[numeroCartaMaquina];
+
+  cartaJogador = meuBaralho[numeroCartaJogador];
+  document.getElementById("campoBatalha").style.display = "none";
+  document.getElementById("btnSortear").style.display = "none";
+  document.getElementById("btnJogar").disabled = false;
+  document.getElementById("escolhaAtributo").style.display = "block";
+
+  minhaCarta.style.display = "block";
+
+  minhaCarta.innerHTML = `
+        <div class='carta'>
+        <div class="nomeCarta">${cartaJogador.nome}</div>
+        <img class="imgCarta" src="${cartaJogador.foto}" alt="">             
+        <div class="atributosCarta">
+        <div class="bg1"><span> </span><span>  ◄</span></div>
+        <div class="bg1"><span> </span><span>  ◄</span></div>
+        <div class="bg1"><span> </span><span>  ◄</span></div>
+        <div class="bg1"><span> </span><span>  ◄</span></div>
+        <div class="bg1"><span> </span><span>  ◄</span></div>
+        <div class="bg1"><span> </span><span>  ◄</span></div></div>`;
+
+  exibirOpcoes();
+}
+
+function exibirOpcoes() {
+  let opcoes = document.getElementById("opcoes");
+  let opcoesTexto = "";
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  for (let atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      ": " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  opcoes.innerHTML = opcoesTexto + tagHTML;
+}
+
+function obtemAtributoSelecionado() {
+  let radioAtributo = document.getElementsByName("atributo");
+  for (let i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+      document.getElementById("btnJogar").disabled = true;
+    }
+  }
+}
+
+let resultado = document.querySelector("#resultado");
+
+function jogar() {
+  let atributoSelecionado = obtemAtributoSelecionado();
+  minhaCarta.innerHTML = "";
+  campoJogador.innerHTML = `
+    <div class='carta'>
+    <div class="nomeCarta">${cartaJogador.nome}</div>
+    <img class="imgCarta" src="${cartaJogador.foto}" alt="">     
+    <div class="atributosCarta">
+    <div class="bg1"><span>Força</span><span>${cartaJogador.atributos.Força}</span></div>
+    <div class="bg2"><span>Inteligência</span><span>${cartaJogador.atributos.Inteligência}</span></div>
+    <div class="bg2"><span>Tecnologia</span><span>${cartaJogador.atributos.Tecnologia}</span></div>
+    <div class="bg2"><span>Velocidade</span><span>${cartaJogador.atributos.Velocidade}</span></div>
+    <div class="bg2"><span>Liderança</span><span>${cartaJogador.atributos.Liderança}</span></div>
+    <div class="bg1"><span>Agilidade</span><span>${cartaJogador.atributos.Agilidade}</span></div>
+    </div></div>`;
+
+  versus.innerHTML = `<img class="img-x" src="https://i.imgur.com/zRf2J4T.png" alt="">`;
+
+  campoMaquina.innerHTML = `
+    <div class='carta'>
+    <div class="nomeCarta">${cartaMaquina.nome}</div>
+    <img class="imgCarta" src="${cartaMaquina.foto}" alt="">    
+    <div class="atributosCarta">
+    <div class="bg1"><span>Força</span><span>${cartaMaquina.atributos.Força}</span></div>
+    <div class="bg2"><span>Inteligência</span><span>${cartaMaquina.atributos.Inteligência}</span></div>
+    <div class="bg1"><span>Tecnologia</span><span>${cartaMaquina.atributos.Tecnologia}</span></div>
+    <div class="bg2"><span>Velocidade</span><span>${cartaMaquina.atributos.Velocidade}</span></div>
+    <div class="bg2"><span>Liderança</span><span>${cartaMaquina.atributos.Liderança}</span></div>
+    <div class="bg2"><span>Agilidade</span><span>${cartaMaquina.atributos.Agilidade}</span></div>
+    </div></div>`;
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    resultado.innerHTML = "Você ganhou!";
+    meuBaralho.push;
+    meuBaralho.push(baralho.splice(numeroCartaMaquina, 1)[0]);
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    resultado.innerHTML = "Você perdeu!";
+    baralho.push(meuBaralho.splice(numeroCartaJogador, 1)[0]);
+  } else {
+    resultado.innerHTML = "Empate";
+  }
+
+  document.getElementById("campoBatalha").style.display = "flex";
+  document.getElementById("escolhaAtributo").style.display = "none";
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnNovoJogo").disabled = false;
+  pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+}
+
+function novoJogo() {
+  if (baralho.length == 0) {
+    resultado.innerHTML = `As cartas do seu oponente acabaram, e você ganhou o jogo.<br> Parabéns!`;
+    baralho = meuBaralho;
+    escodeButton();
+  } else if (meuBaralho.length == 0) {
+    resultado.innerHTML = `As cartas acabaram, e você perdeu o jogo.<br> Boa sorte na próxima vez`;
+    escodeButton();
+  } else {
+    document.getElementById("btnNovoJogo").disabled = true;
+    minhaCarta.style.display = "none";
+    document.getElementById("escolhaAtributo").style.display = "none";
+    campoJogador.innerHTML = "";
+    versus.innerHTML = "";
+    campoMaquina.innerHTML = "";
+    resultado.innerHTML = "";
+    sortearCarta();
+    pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+  }
+}
+
+function reiniciarJogo() {
+  meuBaralho = [];
+  distribuiCartas();
+  pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+  resultado.innerHTML = "";
+  document.getElementById("btnNovoJogo").disabled = true;
+  mostraButton();
+}
+
+function escodeButton() {
+  document.getElementById("reiniciarJogo").style.display = "inline-block";
+  document.getElementById("btnJogar").style.display = "none";
+  document.getElementById("btnNovoJogo").style.display = "none";
+  campoJogador.innerHTML = "";
+  versus.innerHTML = "";
+  campoMaquina.innerHTML = "";
+}
+
+function mostraButton() {
+  document.getElementById("reiniciarJogo").style.display = "none";
+  document.getElementById("btnJogar").style.display = "inline-block";
+  document.getElementById("btnNovoJogo").style.display = "inline-block";
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnSortear").style.display = "inline-block";
+  document.getElementById("reiniciarJogo").style.display = "none";
+}
+
+//popup
+
+function showPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+// Esconde o popup com o Codepen Embed
+function hidePopup(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Não consegui Zera o placar e Reiniciar o jogo
